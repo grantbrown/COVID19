a43f6130ab0a76332535789b4f537b36f39b4a9f
gown <gown@ph-gb.iowa.uiowa.edu>
2020-06-03 10:08:10 -0500

    Tweak renderR0 script

--- Project/GetLatestR0Figure/renderR0.R
+++ Project/GetLatestR0Figure/renderR0.R
@@ -1,10 +1,29 @@
 library(rmarkdown)
 setwd("/home/Analysis/GetLatestR0Figure/")
+
+resultDirectory <- "../../Results/"
+if (!dir.exists(resultDirectory)){
+  resultDirectory <- "/home/basefolder/Results/"
+}
+resultDir <- setdiff(dir(resultDirectory), "placeholder")
+
+## Determine if we're supposed to be working in a specific folder, or grabbing the latest
+if (!exists("resultsDirectoryName")){
+  maxidx <- which.max(as.Date(resultDir))
+  rprtDate <- max(as.Date(resultDir), na.rm = TRUE)
+  #resultsDirectoryName <- paste0(resultDirectory, resultDir[maxidx], "/results")
+} else{
+  rprtDate <- as.Date(resultsDirectoryName)
+  #resultsDirectoryName <- paste0(resultDirectory, resultsDirectoryName, "/results")
+}
+
+
+
 renderEnv <- new.env()
 renderEnv$resultDirectory <- "/home/basefolder/Results/"
 render("LatestR0Information.Rmd", envir = renderEnv)
 infileName <- "LatestR0Information.html"
-outfileName <- paste0(c("R0 Estimates - (", as.character(Sys.Date()), ").html"),
+outfileName <- paste0(c("R0 Estimates - (", as.character(rprtDate), ").html"),
 		      collapse = "")
 outfileName <- paste0("/home/basefolder/Results/", outfileName)
 file.copy(infileName, outfileName, overwrite = TRUE)
@@ -15,7 +34,7 @@ renderEnv <- new.env()
 renderEnv$resultDirectory <- "/home/basefolder/Results/"
 render("LatestInformationAndProjections.Rmd", envir = renderEnv)
 infileName <- "LatestInformationAndProjections.html"
-outfileName <- paste0(c("Latest Projections - (", as.character(Sys.Date()), ").html"),
+outfileName <- paste0(c("Latest Projections - (", as.character(rprtDate), ").html"),
                       collapse = "")
 outfileName <- paste0("/home/basefolder/Results/", outfileName)
 file.copy(infileName, outfileName, overwrite = TRUE)
